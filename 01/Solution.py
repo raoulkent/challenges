2d6c34fac7315602460c4d221a10b9f26c3f5dd5
Raoul Kent <raoulkent@gmail.com>
2018-01-12 13:53:03 +0100

    Day 1, Challenge 1

--- 01/Solution.py
+++ 01/Solution.py
@@ -1,20 +1,36 @@
 from data import DICTIONARY, LETTER_SCORES
 
+
 def load_words():
     """Load dictionary into a list and return list"""
+
     list = open(DICTIONARY).readlines()
     return list
 
-def calc_word_value():
+
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    for
-    pass
 
-def max_word_value():
+    score = []
+    for letters in word:
+        score += LETTER_SCORES.get(letters.upper())
+    return score
+
+
+def max_word_value(words = None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+
+    if not words:
+        words = load_words()
+    scores = map(calc_word_value, words)
+    scorelist = list(scores)
+    maxscore = max(scorelist)
+    maxindex = maxscore.index(scorelist)
+    return words(maxindex)
+
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+    # run unittests to validate
+    calc_word_value('test')
