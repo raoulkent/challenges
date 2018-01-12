cb3a3b90c2d2d7dad6a79a9337b996606e56733a
Raoul Kent <raoulkent@gmail.com>
2018-01-12 14:40:28 +0100

    Day 1, Challenge 1 Now compliant with tests.

--- 01/Solution.py
+++ 01/Solution.py
@@ -4,21 +4,24 @@ from data import DICTIONARY, LETTER_SCORES
 def load_words():
     """Load dictionary into a list and return list"""
 
-    list = open(DICTIONARY).readlines()
-    return list
+    words = []
+    with open(DICTIONARY) as f:
+        for line in f:
+            words.append(line.strip())
+    return words
 
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
 
-    score = []
+    score = 0
     for letters in word:
-        score += LETTER_SCORES.get(letters.upper())
+        score += LETTER_SCORES.get(letters.upper(), 0)
     return score
 
 
-def max_word_value(words = None):
+def max_word_value(words=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
 
@@ -27,10 +30,10 @@ def max_word_value(words = None):
     scores = map(calc_word_value, words)
     scorelist = list(scores)
     maxscore = max(scorelist)
-    maxindex = maxscore.index(scorelist)
-    return words(maxindex)
+    maxindex = scorelist.index(maxscore)
+    return words[maxindex]
 
 
 if __name__ == "__main__":
     # run unittests to validate
-    calc_word_value('test')
+    load_words()
